f2def8dca705001eb51477cae3e38e30df51f367
Kevin8548 <kevinarroyolopez231@gmail.com>
2024-07-10 12:58:18 -0600

    Autores

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms.fields import PasswordField, StringField, SubmitField
 
+
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
@@ -11,24 +12,44 @@ bootstrap = Bootstrap(app)
 def index():
     return render_template('base.html')
 
-@app.route ('/libros')
+@app.route('/libros')
 def libros():
-    #Conectar con la base de datos#
+    # Conectar con la base de datos
     conexion = psycopg2.connect(
-        database="biblioteca3a",
+        database="biblioteca3A",
         user="postgres",
-        password="k888"
-        host="localhost"
+        password="k888",
+        host="localhost",
         port="5432"
     )
-
-    #crear un cursor (objeto para recorrer las tablas)
+    # crear un cursor (objeto para recorrer las tablas)
     cursor = conexion.cursor()
-    #ejecutar una consulta en postgres
-    cursor.execute('''SELECT * FROM libros''')
+    # ejecutar una consulta en postgres
+    cursor.execute('''SELECT * FROM libros_view''')
     #recuperar la informacion
     datos = cursor.fetchall()
-    #cerrar cursor y conexion a la base de datos
+    #cerrar cursos y conexion a la base de datos
     cursor.close()
     conexion.close()
     return render_template('libros.html', datos=datos)
+
+@app.route ('/autores')
+def autores():
+     # Conectar con la base de datos
+    conexion = psycopg2.connect(
+        database="biblioteca3A",
+        user="postgres",
+        password="k888",
+        host="localhost",
+        port="5432"
+    )
+    # crear un cursor (objeto para recorrer las tablas)
+    cursor = conexion.cursor()
+    # ejecutar una consulta en postgres
+    cursor.execute('''SELECT * FROM autores_view''')
+    #recuperar la informacion
+    datos = cursor.fetchall()
+    #cerrar cursos y conexion a la base de datos
+    cursor.close()
+    conexion.close()
+    return render_template('autores.html', datos=datos)
